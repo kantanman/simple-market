8c8b227f2ce1bc334acce257704da16283b6afd0
Eugene <z5258551@unsw.edu.au>
2020-02-15 21:54:07 +1000

    error handling complete

--- market_create.py
+++ market_create.py
@@ -2,7 +2,12 @@ import data_struc
 
 
 def market_create(Min, Max):
+    try:
+        assert (Min < Max) and (Min >= 0)
+    except AssertionError:
+        print ("input error")
     market = data_struc.market(Min, Max)
+    
     data_struc.market_list[market.market_id] = market
 
     
