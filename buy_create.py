72cf752aaf4385a6e88b04e7f99c2039c7536fa2
Eugene <z5258551@unsw.edu.au>
2020-02-15 21:17:17 +1000

    trade matching feature started

--- buy_create.py
+++ buy_create.py
@@ -1,12 +1,20 @@
-import data_struc
+import data_struc as ds
 
 def buy_create(price, quantity, market_id):
-    trade = data_struc.buy_object(price, quantity, market_id)
+
+    #instantiate buy object
+    trade = ds.buy_object(price, quantity, market_id)
     try:
-        data_struc.market_list[market_id].buy.append(trade)
-        #check price here
-        
-    except KeyError:
-        print("market doesnt exist :(")
-    
+        ds.market_list[market_id].buy.append(trade)
+        assert(price >= ds.market_list[market_id].min and price <= ds.market_list[market_id].max)
+    except (KeyError, AssertionError):
+        print("market doesnt exist or price is incorrect")
+
+    #find matching trade 
+    for i in ds.market_list[market_id].sell:
+        if (i.price == price) and (i.quantity == quantity):
+            print("Trade matched with trade_id: {} for {} units at {} dollars".format(i.trade_id, i.quantity, i.price))
+        else:
+            print("submitted trade")
+
     return trade
--- sell_create.py
+++ sell_create.py
@@ -1,10 +1,20 @@
-import data_struc
+import data_struc as ds
 
 def sell_create(price, quantity, market_id):
-    trade = data_struc.sell_object(price, quantity, market_id)
+
+    #instantiate sell object
+    trade = ds.sell_object(price, quantity, market_id)
     try:
-        data_struc.market_list[market_id].buy.append(trade)
-    except KeyError:
-        print("Market doesnt exist :(")
+        ds.market_list[market_id].sell.append(trade)
+        assert(price >= ds.market_list[market_id].min and price <= ds.market_list[market_id].max)
+    except (KeyError, AssertionError):
+        print("market doesnt exist or price is incorrect")
+
+    #find matching trade 
+    for i in ds.market_list[market_id].buy:
+        if (i.price == price) and (i.quantity == quantity):
+            print("Trade matched with trade_id: {} for {} units at {} dollars".format(i.trade_id, i.quantity, i.price))
+        else:
+            print("submitted trade")
 
     return trade
